40c4ba958eabc39121900146bd8bc451be5336ef
Mike <ampgzo2@gmail.com>
2022-03-19 14:02:32 -0400

    fixed issue with column id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,8 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   Product.findAll({
-    include: [{ model: Category },{ model: Tag, through: ProductTag }],
-    attributes: ['id', 'category_name']
+    include: [{ model: Category },{ model: Tag, through: ProductTag }]
   })
   .then(dbProductData => res.json(dbProductData))
     .catch(err => {
