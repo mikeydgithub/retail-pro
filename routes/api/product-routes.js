be5515837d71f24cd89c84a19c2b152e68284089
Mike <ampgzo2@gmail.com>
2022-03-19 16:26:19 -0400

    finish testing all routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -57,7 +57,7 @@ router.post('/', (req, res) => {
   })
   .then((product) => {
     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-    if (req.body.tagIds.length) {
+    if (req.body.tagIds) {
       const productTagIdArr = req.body.tagIds.map((tag_id) => {
         return {
           product_id: product.id,
@@ -86,7 +86,7 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({ where: { product_id: req.params.id, tag_id: req.params.id } });
     })
     .then((productTags) => {
       // get list of current tag_ids
